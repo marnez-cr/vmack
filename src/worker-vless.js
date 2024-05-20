518e69fbb4d397a558ebc4e3341978b1635329df
MARNEZ <110733205+marnez-cr@users.noreply.github.com>
2024-05-20 17:43:53 +0700

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '0ba21437-67c0-4f72-95aa-0f3496358d1d';
 
-let proxyIP = '';
+let proxyIP = 'proxy-sg.tidur.cfd';
 
 
 if (!isValidUUID(userID)) {
